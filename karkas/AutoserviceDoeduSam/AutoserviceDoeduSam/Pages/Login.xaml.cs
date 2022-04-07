b734142b9f26a855297084b7b2be7a3b10e28f13
Your Name <you@example.com>
2022-04-07 19:24:19 +0300

    j

---
+++ karkas/AutoserviceDoeduSam/AutoserviceDoeduSam/Pages/Login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AutoserviceDoeduSam.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoserviceDoeduSam.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            Class.Manager.MainFrame.Navigate(new Pages.Admin());
+        }
+    }
+}
