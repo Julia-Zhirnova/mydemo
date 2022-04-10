ccf873e27fe8ff7ad29095a9223141ab1e19ce44
Your Name <you@example.com>
2022-04-11 01:27:44 +0300

    s

---
+++ spisoc/AutoserviceDoeduSam/AutoserviceDoeduSam/Pages/Admin.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoserviceDoeduSam.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Admin.xaml
+    /// </summary>
+    public partial class Admin : Page
+    {
+        
+        public Admin()
+        {
+            InitializeComponent();
+
+            
+
+            var checkDiscount = Model.autoserviceEntities.GetContext().Service.ToList();
+            checkDiscount.Insert(0, new Model.Service
+            {
+                Discount = 0
+            });
+            ComboType.ItemsSource = checkDiscount;
+
+            CheckActual.IsChecked = true;
+            ComboType.SelectedIndex = 0;
+            UpdateServices();
+        }
+        private void UpdateServices()
+        {
+            var currentServices = Model.autoserviceEntities.GetContext().Service.ToList();
+            if (ComboType.SelectedIndex > 0)
+                currentServices = currentServices.Where(p => p.Discount.HasValue).ToList();
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            LViewService.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Class.Manager.MainFrame.Navigate(new Pages.EditService());
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var RemoveColums = LViewService.SelectedItems.Cast<Model.Service>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить {RemoveColums.Count()} элементов?", "Внимание!",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Model.autoserviceEntities.GetContext().Service.RemoveRange(RemoveColums);
+                    Model.autoserviceEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+
+                    LViewService.ItemsSource = Model.autoserviceEntities.GetContext().Service.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Btn_Add(object sender, RoutedEventArgs e)
+        {
+            Class.Manager.MainFrame.Navigate(new Pages.EditService());
+        }
+    }
+}
---
+++ spisoc/AutoserviceDoeduSam/AutoserviceDoeduSam/Windows/MainAutoserviceWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoserviceDoeduSam.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainAutoserviceWindow.xaml
+    /// </summary>
+    public partial class MainAutoserviceWindow : Window
+    {
+        public MainAutoserviceWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Pages.ServicePage());
+            Class.Manager.MainFrame = MainFrame;
+            
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Class.Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (Class.Manager.MainFrame.CanGoBack) Back.Visibility = Visibility.Visible;
+            else Back.Visibility = Visibility.Hidden;
+            
+            
+            
+        }
+
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            Class.Manager.MainFrame.Navigate(new Pages.Avtorisation());
+        }
+
+        private void Polzov_Click(object sender, RoutedEventArgs e)
+        {
+            Class.Manager.MainFrame.Navigate(new Pages.ServicePage());
+        }
+    }
+}
